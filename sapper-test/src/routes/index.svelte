284e886926a87259e3ce5269643f174b4934da3b
Utilisateur <franck.ledoux.pro@gmail.com>
2020-02-04 20:12:38 +0100

    slatter: posts list

---
+++ sapper-test/src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  export async function preload(page, session) {
+    const res = await this.fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const text = await res.text();
+    return { posts: JSON.parse(text) };
+  }
+</script>
+
+<script>
+  import Grid from "../components/posts/Grid.svelte";
+  import Cell from "../components/posts/Cell.svelte";
+  import Post from "../components/posts/Post.svelte";
+
+  export let posts;
+</script>
+
+<main>
+  <Grid>
+    {#each posts as post (post.id)}
+      <Cell>
+        <Post {post} />
+      </Cell>
+    {/each}
+  </Grid>
+</main>
