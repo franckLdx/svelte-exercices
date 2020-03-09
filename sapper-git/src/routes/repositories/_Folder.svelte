57f74f4a1ac605284a012c844559676724d22657
Utilisateur <franck.ledoux.pro@gmail.com>
2020-03-09 22:14:07 +0100

    update folder layout

--- sapper-git/src/routes/repositories/_Folder.svelte
+++ sapper-git/src/routes/repositories/_Folder.svelte
@@ -1,9 +1,18 @@
 <script>
+  import { parseISO, format } from "date-fns";
   import { getLastCommit } from "@Services/commit";
+  import Icon from "svelte-awesome/components/Icon.svelte";
+  import folder from "svelte-awesome/icons/folder-open-o";
+  import file from "svelte-awesome/icons/file-o";
 
   export let entries;
 
   let width;
+
+  function formatDate(date) {
+    const dateUTC = parseISO(date);
+    return format(dateUTC, "yyyy/MM/dd hh:mm a");
+  }
 </script>
 
 <style>
@@ -19,9 +28,20 @@
 <div class="border border-dark pt-2 pl-2 pr-2 mb-2" bind:clientWidth={width}>
   {#each entries as entry (entry.oid)}
     <div class="item border border-dark mb-2 p-2 row no-gutters">
-      <span class="col col-xs-12 col-sm-6 col-lg-4">{entry.name}</span>
+      <span class="col col-xs-12 col-sm-6 col-lg-4">
+        {#if entry.type === 'tree'}
+          <span class="mr-2">
+            <Icon data={folder} scale="1.25" />
+          </span>
+        {:else}
+          <span class="mr-2">
+            <Icon data={file} scale="1" />
+          </span>
+        {/if}
+        {entry.name}
+      </span>
       <span class="col col-sm-6 col-lg-3" hidden={width < 576}>
-        {entry.commit.committedDate}
+        {formatDate(entry.commit.committedDate)}
       </span>
       <span class="col" hidden={width <= 920}>{entry.commit.message}</span>
     </div>
