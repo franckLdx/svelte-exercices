3861eab41cf01a40df1da5f7c86b5ca0a51f2653
Utilisateur <franck.ledoux.pro@gmail.com>
2020-03-20 23:24:35 +0100

    display file content

--- sapper-git/src/components/File.svelte
+++ sapper-git/src/components/File.svelte
@@ -1,14 +1,20 @@
 <script>
   export let content;
+
+  function format(raw) {
+    let result = raw;
+    result = result.replace(/</g, "&lt;");
+    return result
+      .replace(/\r\n/g, "<br/>")
+      .replace(/\n/g, "<br/>")
+      .replace(/\r/g, "<br/>")
+      .replace(/\s/g, "&nbsp");
+  }
 </script>
 
 <article class="card">
   <div class="card-body">
-    {@html content
-      .replace(/\r\n/g, '<br/>')
-      .replace(/\n/g, '<br/>')
-      .replace(/\r/g, '<br/>')
-      .replace(/\s/g, '&nbsp')}
+    {@html format(content)}
   </div>
 </article>
 <svelte:options immutable />
