f36b8e7f3ff0ed563ce538b81b58db7091925e3d
Utilisateur <franck.ledoux.pro@gmail.com>
2020-04-19 19:08:55 +0200

    add ship page starter

---
+++ sapper-space-x/src/routes/ships/[id].svelte
@@ -0,0 +1,11 @@
+<script context="module">
+  export async function preload(page) {
+    return { ship: { name: "Not yet" } };
+  }
+</script>
+
+<script>
+  export let ship;
+</script>
+
+{ship.name}
